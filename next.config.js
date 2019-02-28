11295e80f6052f5697892467ee27369f144696db
Zeh Fernando <zeh@zehfernando.com>
2019-02-27 21:16:02 -0500

    Chore: added @zeit/next-typescript to use Typescript [not working]
    Some dependencies (next, React) also had to be updated to their latest version.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const withTypescript = require('@zeit/next-typescript')
+
+module.exports = withTypescript({
   exportPathMap: function() {
     return {
       '/': { page: '/' }
     }
   }
-}
+})
