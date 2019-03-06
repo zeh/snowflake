d22499540723f7906e94da31b25997e93093b3ff
Zeh Fernando <zeh@work.co>
2019-03-06 15:13:21 -0500

    Chore: added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:react/recommended",
 		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
 	],
 	rules: {
 		"no-dupe-class-members": ["off"],
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports =  {
+	trailingComma: "all",
+	printWidth: 120,
+	useTabs: true,
+	arrowParens: "always",
+	endOfLine: "lf",
+};
