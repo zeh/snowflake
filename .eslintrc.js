fdf57509106a37dad710e0f1fbe8b065ada4ea61
Zeh Fernando <zeh@work.co>
2019-03-06 15:01:03 -0500

    Chore: added eslint (605 errors)

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	env: {
+		browser: true,
+		es6: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: "module",
+		useJSXTextNode: true,
+		project: "./tsconfig.json",
+		tsconfigRootDir: "./",
+		extraFileExtensions: [],
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	rules: {
+		"no-dupe-class-members": ["off"],
+		"indent": ["off"],
+		"@typescript-eslint/indent": ["error", "tab"],
+		"@typescript-eslint/explicit-function-return-type": ["error"],
+		"@typescript-eslint/interface-name-prefix": ["error", "always"],
+		"@typescript-eslint/array-type": ["error", "array-simple"],
+		"react/display-name": ["off"],
+		"react/jsx-curly-brace-presence": ["error", { props: "always" }],
+		"jsx-quotes": ["error", "prefer-double"],
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
