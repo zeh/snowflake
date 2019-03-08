e3cad46642757b6d0ecab4d9cf1553e16e83360c
Zeh Fernando <zeh@work.co>
2019-03-08 17:01:42 -0500

    Refactor: using flexbox to render track selector

--- src/components/TrackSelector.tsx
+++ src/components/TrackSelector.tsx
@@ -4,27 +4,37 @@ import Score from "../ladder/models/Score";
 import Ladder from "../ladder/models/Ladder";
 
 const styles = {
-	table: {
+	container: {
 		width: "100%",
-		borderSpacing: 3,
-		borderBottom: "2px solid #ccc",
+		display: "flex",
 		paddingBottom: 20,
 		marginBottom: 20,
-		marginLeft: -3,
+		marginTop: 20,
+		marginRight: -3,
+		borderBottom: "2px solid #ccc",
 	},
-	trackSelectorValue: {
+	category: {
+		display: "flex",
+		flex: "1",
+	},
+	track: {
+		flex: "1",
+		marginRight: 3,
+		alignSelf: "flex-end",
+	},
+	trackName: {
+		textAlign: "center" as "center",
+		fontSize: 9,
+		marginBottom: 5,
+	},
+	trackBox: {
 		lineHeight: "50px",
-		width: 50,
 		textAlign: "center" as "center",
 		background: "#eee",
 		fontWeight: "bold" as "bold",
 		fontSize: 24,
 		borderRadius: 3,
 		cursor: "pointer" as "pointer",
-	},
-	trackSelectorLabel: {
-		textAlign: "center" as "center",
-		fontSize: 9,
 	},
 };
 
@@ -37,43 +47,31 @@ interface IProps {
 
 class TrackSelector extends React.Component<IProps> {
 	public render(): JSX.Element {
-		const allTracks = this.props.ladder.getAllTracks();
+		const { focusedTrackId, ladder, score } = this.props;
+
 		return (
-			<table css={styles.table}>
-				<tbody>
-					<tr>
-						{allTracks.map((track) => (
-							<td
-								key={track.id}
-								css={styles.trackSelectorLabel}
-								onClick={() => this.props.setFocusedTrackIdFn(track.id)}
-							>
-								{track.name}
-							</td>
+			<div css={styles.container}>
+				{ladder.categories.map((category) => (
+					<div key={category.id} css={styles.category}>
+						{category.tracks.map((track) => (
+							<div key={track.id} css={styles.track} onClick={() => this.props.setFocusedTrackIdFn(track.id)}>
+								<div css={styles.trackName}>{track.name}</div>
+								<div
+									css={styles.trackBox}
+									style={{
+										border:
+											"4px solid " + (track.id == focusedTrackId ? "#000" : ladder.getCategoryColorForTrack(track.id)),
+										background: ladder.getCategoryColorForTrack(track.id),
+										color: ladder.getCategoryTextColorForTrack(track.id),
+									}}
+								>
+									{score.getTrackMilestone(track.id)}
+								</div>
+							</div>
 						))}
-					</tr>
-					<tr>
-						{allTracks.map((track) => (
-							<td
-								key={track.id}
-								css={styles.trackSelectorValue}
-								style={{
-									border:
-										"4px solid " +
-										(track.id == this.props.focusedTrackId
-											? "#000"
-											: this.props.ladder.getCategoryColorForTrack(track.id)),
-									background: this.props.ladder.getCategoryColorForTrack(track.id),
-									color: this.props.ladder.getCategoryTextColorForTrack(track.id),
-								}}
-								onClick={() => this.props.setFocusedTrackIdFn(track.id)}
-							>
-								{this.props.score.getTrackMilestone(track.id)}
-							</td>
-						))}
-					</tr>
-				</tbody>
-			</table>
+					</div>
+				))}
+			</div>
 		);
 	}
 }
